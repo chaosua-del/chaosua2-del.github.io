5d9e971c56073a33c278702a27814ee60d7cf7e6
chaosua-del <54207425+chaosua-del@users.noreply.github.com>
2020-03-13 11:32:40 +0200

    feat: scollUp button is ready now

--- repair-desaign/src/js/main.js
+++ repair-desaign/src/js/main.js
@@ -1,15 +1,37 @@
 $(document).ready(function () {
+  // script for modal window
   var modal = $('.modal'),
     modalBtn = $('.button'),
     closeBtn = $('.modal__close');
 
-  console.log(modalBtn);
-
+  // on click we toggle class for modal 
   modalBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
 
+  // on click we toggle class for modal
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+
+  // scrollUp
+  $(window).scroll(function () {
+    if ($(this).scrollTop()) {
+      $('#toTop').fadeIn();
+    } else {
+      $('#toTop').fadeOut();
+    }
+  });
+
+  $("#toTop").click(function () {
+    //1 second of animation time
+    //html works for FFX but not Chrome
+    //body works for Chrome but not FFX
+    //This strange selector seems to work universally
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000);
+  });
+
 });
