3e24fbb2185403e32b51c4f52c5544bcbcb76390
chaosua-del <54207425+chaosua-del@users.noreply.github.com>
2020-04-01 23:05:31 +0300

    new section  advantages

---
+++ saytspb/src/js/main.js
@@ -0,0 +1,6 @@
+// $(document).ready(function () {
+//   $('.burger-menu').on('click', function () {
+//     $('.menu__nav').toggleClass('menu__nav--active');
+//     $(this).toggleClass('burger-menu--active')
+//   });
+// });
