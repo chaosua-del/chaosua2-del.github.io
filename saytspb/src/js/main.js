d1c66bf4d6242ee80533514419118a07d02c4e3b
chaosua-del <54207425+chaosua-del@users.noreply.github.com>
2020-04-02 22:37:25 +0300

    feat: new section feedback

--- saytspb/src/js/main.js
+++ saytspb/src/js/main.js
@@ -1,6 +1,20 @@
-// $(document).ready(function () {
-//   $('.burger-menu').on('click', function () {
-//     $('.menu__nav').toggleClass('menu__nav--active');
-//     $(this).toggleClass('burger-menu--active')
-//   });
-// });
+$(document).ready(function () {
+
+  var swiper = new Swiper('.cases__swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var swiper2 = new Swiper('.feedback__swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+});
